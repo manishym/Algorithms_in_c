39acb54ee9edd1077c0f806240e0bd0860d7fc15
Manish M Yathnalli <manish.ym@gmail.com>
2014-12-30 12:59:10 +0530

    Closest pair by brute force.

--- lib/seed.c
+++ lib/seed.c
@@ -1,12 +1,50 @@
 #include "seed.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#ifndef __APPLE__
+       #include <sys/types.h> 
+       #include <sys/stat.h>  
+       #include <fcntl.h>     
 
+int seed_random(){
+	int fd = open("/dev/random", O_RDONLY);
+	int ret, seed ; 
+	time_t now ;
+	
+	if(fd < 0) {
+		perror ("open(): ");
+
+		goto bail;
+	}
+	
+	ret = read(fd, &seed, sizeof(int));
+	if(ret < 0) {
+		perror( "read(): " );
+		goto bail;
+	}
+	srand(seed);
+	return 0;
+bail:
+	time(&now);
+	srand(now);
+	return -1;
+
+}
+#endif
 int seed_array(int array[], int max, int length) {
 	int i;
+#ifdef __APPLE__
 	srandomdev();
+#else
+	seed_random();
+#endif
+	
 	for(i = 0; i < length; i++){
 		array[i] = (int)random() % max;
 	}
+	srand(array[5]);
     
 
 }
